821caffb9f4dcca326d20647b30524e25b1f22d4
Danlon Situma <situmadanlon@gmail.com>
2021-04-07 10:12:43 +0300

    Add modified approuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component'
+import { RepositoryComponent } from './repository/repository.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/profile', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'repository', component: RepositoryComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
